e68e5a2bf29d03f99a873dd0c542676f09e0e9c6
Lucas Monteiro <lucasggtyy358@gmail.com>
2026-05-16 10:53:33 -0300

    Email e telefone feitos

--- regex.py
+++ regex.py
@@ -12,8 +12,13 @@ print(cpf_er.match(cpf))
  
 # E-mail
 
+email_er = re.compile(r'^[\w._-]{2,}@[\w._-]+\.[a-z]{3}(\.(br|ao|pt|es|de|uk))?$')
+email = input("Digite seu e-mail: ")
+print(email_er.match(email))
 
 # Telefone
-
+telefone_er = re.compile(r'^(\d{11}|\(\d{2}\)\d{5}-\d{4})$')
+telefone = input("Digite seu telefone: ")
+print(telefone_er.match(telefone))
 
 # Extraindo e-mails validos de um texto
